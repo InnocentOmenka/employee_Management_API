7cb4310f1ff8b9ac17e251cf3dfed3c0222beeca
InnocentOmenka <innocent.omenka@decagon.dev>
2023-02-03 20:36:32 +0100

    employee system deployment

---
+++ src/main/java/com/inno/employeesystemapi/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.inno.employeesystemapi.repositories;
+
+import com.inno.employeesystemapi.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
